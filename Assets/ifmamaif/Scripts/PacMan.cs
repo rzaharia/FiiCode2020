48e14fc8c92600ffdd49bcf84bb3dd3d2663a82e
maify <ifmamaif@gmail.com>
2020-03-06 17:58:03 +0200

    Updated scripts and scene

--- Assets/ifmamaif/Scripts/PacMan.cs
+++ Assets/ifmamaif/Scripts/PacMan.cs
@@ -1,74 +1,133 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
-public class PacMan : MonoBehaviour {
+public class pacman : MonoBehaviour
+{
+    #region Types
+    private enum TypeCell
+    {
+        Empty,
+        Point,
+        PowerUp,
+        Portal,
+        Wall,
 
-	public float speed = 4.0f;
+    };
+    #endregion
 
-	private Vector2 direction = Vector2.zero;
+    #region Members
+    private readonly string[] pacmanMap = new string[] {
+" _____________________________________________________"  ,
+"| . . . . . . . . . . . . | | . . . . . . . . . . . . |" ,
+"| .  _____  .  _______  . | | .  _______  .  _____  . |" ,
+"| . |     | . |       | . | | . |       | . |     | . |" ,
+"| o |_____| . |_______| . |_| . |_______| . |_____| o |" ,
+"| . . . . . . . . . . . . . . . . . . . . . . . . . . |" ,
+"| .  _____  .  _  .  _____________  .  _  .  _____  . |" ,
+"| . |_____| . | | . |_____   _____| . | | . |_____| . |" ,
+"| . . . . . . | | . . . . | | . . . . | | . . . . . . |" ,
+"|_________  . | |_____  . | | .  _____| | .  _________|" ,
+"          | . |  _____|   |_|   |_____  | . |          " ,
+"          | . | |                     | | . |          " ,
+"          | . | |    _____   _____    | | . |          " ,
+"__________| . |_|   |             |   |_| . |_________ " ,
+"[           .       |             |       .          ] " ,
+"__________  .  _    |             |    _  .  _________ " ,
+"          | . | |   |_____________|   | | . |          " ,
+"          | . | |                     | | . |          " ,
+"          | . | |    _____________    | | . |          " ,
+" _________| . |_|   |_____   _____|   |_| . |_________ " ,
+"| . . . . . . . . . . . . | | . . . . . . . . . . . . |" ,
+"| .  _____  .  _______  . | | .  _______  .  _____  . |" ,
+"| . |___  | . |_______| . |_| . |_______| . |  ___| . |" ,
+"| o . . | | . . . . . . . . . . . . . . . . | | . . o |" ,
+"|___  . | | .  _  .  _____________  .  _  . | | .  ___|" ,
+"|___| . |_| . | | . |_____   _____| . | | . |_| . |___|" ,
+"| . . . . . . | | . . . . | | . . . . | | . . . . . . |" ,
+"| .  _________| |_____  . | | .  _____| |_________  . |" ,
+"| . |_________________| . |_| . |_________________| . |" ,
+"| . . . . . . . . . . . . . . . . . . . . . . . . . . |" ,
+"|_____________________________________________________|" ,
+};
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private Dictionary<char, TypeCell> keyValues;
+    private TypeCell[,] terrain;
+    private GameObject[,] GameObjects = null;
 
-		CheckInput ();
+    public Texture2D asciiTable;
+    public int texelSize = 64;
+    #endregion
 
-		Move ();
 
-		UpdateOrientation ();
-	}
+    Sprite GetSprite(int x,int y)
+    {
+        //  return Sprite.Create(asciiTable, new Rect(texelSize * x / asciiTable.width, texelSize * y / asciiTable.height, texelSize, texelSize), new Vector2(0.5f, 0.5f));
+        return Sprite.Create(asciiTable, new Rect(x*texelSize,y* texelSize, texelSize, texelSize), new Vector2(0.5f, 0.5f));
+    }
 
-	void CheckInput () {
+    // Start is called before the first frame update
+    void Start()
+    {
+        // keyValues.Add('|', TypeCell.Wall);
+        // keyValues.Add('_', TypeCell.Wall);
+        // keyValues.Add('.', TypeCell.Point);
+        // keyValues.Add(' ', TypeCell.Empty);
+        // keyValues.Add('o', TypeCell.PowerUp);
+        // keyValues.Add('[', TypeCell.Portal);
+        // keyValues.Add(']', TypeCell.Portal);
+        //
+        // terrain = new TypeCell[pacmanMap.Length, pacmanMap[0].Length];
+        // for (int i = 0; i < pacmanMap.Length; i++)
+        //     for (int j = 0; j < pacmanMap[i].Length; j++)
+        //     {
+        //         terrain[i, j] = keyValues[pacmanMap[i][j]];
+        //     }
+        //
+        // GameObjects = new GameObject[pacmanMap.Length, pacmanMap[0].Length];
+        // GameObject preObject = new GameObject();
+        // preObject.AddComponent<SpriteRenderer>();
 
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 
-			direction = Vector2.left;
 
-		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+        //Text textComponenet = this.GetComponent<Text>();
+        //string text = "";
+        //foreach(string elem in pacmanMap)
+        //{
+        //    text += elem+"\n";
+        //}
+        //textComponenet.text = text;
 
-			direction = Vector2.right;
 
-		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+        //GameObject gameObject = new GameObject();
+        //gameObject.transform.SetParent(this.transform);
+        //SpriteRenderer spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        //spriteRenderer.sprite = Sprite.Create(asciiTable, new Rect(0, 0, asciiTable.width, asciiTable.height), new Vector2(0.5f, 0.5f));
 
-			direction = Vector2.up;
+        GameObjects = new GameObject[16, 16];
 
-		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+        // int i = 2, j = 2;
+        int scale = 50;
+        int size = 32;
+        for (int i = 0; i < 16; i++)
+            for (int j = 0; j < 16; j++)
+            {
+                GameObjects[i, j] = new GameObject(i + " " + j);
+                GameObject gameObject = GameObjects[i, j];
+                gameObject.transform.SetParent(this.transform);
+                gameObject.transform.localScale = new Vector3(scale, scale, 0);
+                gameObject.transform.localPosition = new Vector3((i-8)* size, (j-8)* size, 0);
+                SpriteRenderer spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+                spriteRenderer.sprite = GetSprite(i, j);
+                
+            }
+    }
 
-			direction = Vector2.down;
-		}
-	}
-
-	void Move () {
-
-		transform.localPosition += (Vector3)(direction * speed) * Time.deltaTime;
-	}
-
-	void UpdateOrientation () {
-
-		if (direction == Vector2.left) {
-
-			transform.localScale = new Vector3 (-1, 1, 1);
-			transform.localRotation = Quaternion.Euler (0, 0, 0);
-
-		} else if (direction == Vector2.right) {
-
-			transform.localScale = new Vector3 (1, 1, 1);
-			transform.localRotation = Quaternion.Euler (0, 0, 0);
-
-		} else if (direction == Vector2.up) {
-
-			transform.localScale = new Vector3 (1, 1, 1);
-			transform.localRotation = Quaternion.Euler (0, 0, 90);
-
-		} else if (direction == Vector2.down) {
-
-			transform.localScale = new Vector3 (1, 1, 1);
-			transform.localRotation = Quaternion.Euler (0, 0, 270);
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
